a12f39b4f6b0972e24cc158f1836262f565db88d
FedericoFdez <fa.fernandez@alumnos.upm.es>
2015-07-09 18:17:41 +0200

    Creada app simple_form

--- pruebafiware/urls.py
+++ pruebafiware/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
 	url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^simpleform/', include('simple_form.urls', namespace="simple_form")),
 ]
