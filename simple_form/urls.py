348dbd137e6d5ad78a77b12090a7754685cb0849
FedericoFdez <fa.fernandez@alumnos.upm.es>
2015-07-13 18:17:53 +0200

    Minor changes

--- simple_form/urls.py
+++ simple_form/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.get_name, name='name'),
+    url(r'^$', views.get_message, name='message'),
 ]
--- simple_form/views.py
+++ simple_form/views.py
@@ -3,24 +3,25 @@ from django.http import HttpResponseRedirect
 
 from .forms import MessageForm
 
-def get_name(request):
+def get_message(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = MessageForm(request.POST)
         # check whether it's a valid:
         if form.is_valid():
-            request.session['last_message'] = form.cleaned_data['message']
             # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return render(request, 'simple_form/result.html', {'form': form,'result': request.session['last_message']})
+            request.session['last_message'] = form.cleaned_data['message']
 
+            # render result
+            return render(request, 'simple_form/result.html', {'form': MessageForm(),'result': request.session['last_message']})
+
+    # if GET and cookie exists, render result
     elif 'last_message' in request.session:
         form = MessageForm()
         return render(request, 'simple_form/result.html', {'form': form,'result': request.session['last_message']})
 
-    # if a GET (or any other method) we'll create a blank form
+    # if GET (or any other method) and no cookie we'll create a blank form
     else:
         form = MessageForm()
 
