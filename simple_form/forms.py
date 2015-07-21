d8b3c8351b4947e8e472eb6e024d2844f119d0e8
FedericoFdez <fa.fernandez@alumnos.upm.es>
2015-07-21 17:58:59 +0200

    Form customized and other tiny changes. New styles added

--- simple_form/forms.py
+++ simple_form/forms.py
@@ -2,5 +2,5 @@ from django import forms
 from nocaptcha_recaptcha.fields import NoReCaptchaField
 
 class MessageForm(forms.Form):
-    message = forms.CharField(label="Write Message", max_length=100)
-    captcha = NoReCaptchaField(label='')
+    message = forms.CharField(label="Write Message", max_length=100, error_messages={'required': 'Please, enter a message.'})
+    captcha = NoReCaptchaField(label='', error_messages={'required': 'Captcha validation is required'})
--- simple_form/views.py
+++ simple_form/views.py
@@ -5,6 +5,8 @@ from django.core.urlresolvers import reverse
 
 from .forms import MessageForm
 
+from time import gmtime, strftime
+
 # Now we're using Class-based views
 
 class FirstMessageView(FormView):
@@ -14,13 +16,14 @@ class FirstMessageView(FormView):
 
     def render_to_response(self, context):
         # if there is a cookie, redirect to KnownUserView
-        if 'last_message' in self.request.session:
+        if ('last_message' and 'date') in self.request.session:
             return HttpResponseRedirect('/simpleform/known')
         return super(FirstMessageView, self).render_to_response(context)      
 
     def form_valid(self, form):
         # save field in user cookie
         self.request.session['last_message'] = form.cleaned_data['message']
+        self.request.session['date'] = strftime("%A, %d %b %Y %H:%M:%S", gmtime())
         return super(FirstMessageView, self).form_valid(form)
 
 class KnownUserView(FormView):
@@ -30,18 +33,20 @@ class KnownUserView(FormView):
 
     def render_to_response(self, context):
         # if there is no cookie, redirect to FirstMessageView
-        if not 'last_message' in self.request.session:
+        if not ('last_message' and 'date') in self.request.session:
             return HttpResponseRedirect('/simpleform')
         return super(KnownUserView, self).render_to_response(context)
 
     def form_valid(self, form):
         # update field in user cookie
         self.request.session['last_message'] = form.cleaned_data['message']
+        self.request.session['date'] = strftime("%A, %d %b %Y %H:%M:%S", gmtime())
         return super(KnownUserView, self).form_valid(form)
 
     def get_context_data(self, **kwargs):
         # save field in context for the template to render correctly
         context = super(KnownUserView, self).get_context_data(**kwargs)
-        if 'last_message' in self.request.session:
-            context['result'] = self.request.session['last_message']
+        if ('last_message' and 'date') in self.request.session:
+            context['message'] = self.request.session['last_message']
+            context['date'] = self.request.session['date']
         return context
